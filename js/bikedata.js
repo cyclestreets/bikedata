c884b31a568b4b1874d6339c95866b5b23a5bf2c
mvl22 <martin@lucas-smith.co.uk>
2016-09-25 14:35:46 +0200

    Move JS code to new JS application class file

--- js/bikedata.js
+++ js/bikedata.js
@@ -1,26 +1,55 @@
-
-// create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-// add an OpenStreetMap tile layer
-var tileUrl = 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png';
-L.tileLayer(tileUrl, {
-	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// add a marker in the given location, attach some popup content to it and open the popup
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-    .openPopup();
-
-
-// add a marker in the given location, attach some popup content to it and open the popup
-L.marker([51.52, -0.092]).addTo(map)
-    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-    .openPopup();
-
-// add a marker in the given location, attach some popup content to it and open the popup
-L.marker([51.51, -0.125]).addTo(map)
-    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-    .openPopup();
-
+// Bike data application code
+bikedata = (function ($) {
+	
+	'use strict';
+	
+	// Internal class properties
+	var _settings = {};
+	
+	
+	return {
+		
+	// Public functions
+		
+		// Main function
+		initialise: function (settings)
+		{
+			// Obtain the settings
+			_settings = settings;
+			
+			// Create the map
+			bikedata.createMap ();
+			
+			// Load the tabs
+			bikedata.loadTabs ();
+			
+		},
+		
+		
+		// Create the map
+		createMap: function ()
+		{
+			// create a map in the "map" div, set the view to a given place and zoom
+			var map = L.map('map').setView([51.505, -0.09], 13);
+			
+			// add an OpenStreetMap tile layer
+			var tileUrl = 'https://tile.cyclestreets.net/opencyclemap/{z}/{x}/{y}.png';
+			L.tileLayer(tileUrl, {
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors; <a href="https://www.thunderforest.com/">Thunderforest</a>'
+			}).addTo(map);
+			
+			// add a marker in the given location, attach some popup content to it and open the popup
+			L.marker([51.5, -0.09]).addTo(map)
+			    .bindPopup('A pretty CSS3 popup.<br />Easily customizable.')
+			    .openPopup();
+			
+		},
+		
+		
+		// Function to load the tabs
+		loadTabs: function ()
+		{
+			$('nav').tabs();
+		}
+	}
+} (jQuery));
