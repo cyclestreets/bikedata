6c419c9743289621a1e16429d9b0a5b5470b0ea1
mvl22 <martin@lucas-smith.co.uk>
2017-04-14 22:35:36 +0100

    Ditto, commented out

--- js/bikedata.js
+++ js/bikedata.js
@@ -755,11 +755,13 @@ var bikedata = (function ($) {
 					// Stop data loading spinner for this layer
 					$('#selector li.' + layerId + ' img.loading').hide();
 					
+					/* Commented out as can be obtrusive if an API endpoint is slow/down
 					// Catch cases of being unable to access the server, e.g. no internet access; avoids "Unexpected token u in JSON at position 0" errors
 					if (jqXHR.status == 0) {
 						vex.dialog.alert ('Error: Could not contact the server; perhaps your internet connection is not working?');
 						return;
 					}
+					*/
 					
 					// Show error, unless deliberately aborted
 					if (jqXHR.statusText != 'abort') {
